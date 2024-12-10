5bba78bf5d9bce43978e3092dbaafbb8f65d6699
jhs129 <john.h.schneider@gmail.com>
2024-12-09 20:42:58 -0500

    updated activities pictures

--- src/components/TripDetails.tsx
+++ src/components/TripDetails.tsx
@@ -1,4 +1,7 @@
 import { FaPlane, FaHotel, FaListUl } from 'react-icons/fa'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 interface FlightInfo {
   departure: string
@@ -15,6 +18,11 @@ interface HotelInfo {
   image: string
 }
 
+interface ActivityInfo {
+  text: string
+  image: string
+}
+
 interface TripDetailsProps {
   title: string
   image: string
@@ -23,10 +31,20 @@ interface TripDetailsProps {
     return: FlightInfo
   }
   hotel: HotelInfo
-  activities: string[]
+  activities: ActivityInfo[]
 }
 
 export default function TripDetails({ title, image, flights, hotel, activities }: TripDetailsProps) {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    swipe: true,
+  }
+
   return (
     <div className="space-y-8">
       <div className="relative w-full h-[400px]">
@@ -89,11 +107,22 @@ export default function TripDetails({ title, image, flights, hotel, activities }
         <h2 className="text-2xl font-semibold mb-4 flex items-center">
           <FaListUl className="mr-2" /> Key Activities
         </h2>
-        <ul className="list-disc pl-5">
-          {activities.map((activity, index) => (
-            <li key={index}>{activity}</li>
-          ))}
-        </ul>
+        <div className="max-w-3xl mx-auto">
+          <Slider {...sliderSettings}>
+            {activities.map((activity, index) => (
+              <div key={index} className="px-2">
+                <div className="aspect-w-16 aspect-h-9 mb-4">
+                  <img
+                    src={activity.image}
+                    alt={activity.text}
+                    className="w-full h-[300px] object-cover rounded-lg"
+                  />
+                </div>
+                <p className="text-center text-lg">{activity.text}</p>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   )
