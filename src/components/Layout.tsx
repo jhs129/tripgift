846e4bd59821e3d9a0f03ed40ede493797609715
jhs129 <john.h.schneider@gmail.com>
2024-12-10 00:04:39 -0500

    updated header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -8,15 +9,25 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Head>
-        <title>A Very Boopadoop Christmas</title>
+        <title>Boopmas.org |A Very Boopadoop Christmas</title>
         <meta name="description" content="A special gift of three weekend trips" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="min-h-screen flex flex-col">
         <header className="bg-primary py-6 mb-8">
-          <div className="container mx-auto px-4 text-center text-white">
-            <h1 className="text-5xl font-bold mb-2 font-christmas">
-              Merry Christmas Boopadoop! 🎄
+          <div className="container mx-auto text-center text-light md:flex md:items-center lg:gap-16">
+            <div className="px-4">
+              <Image
+                src="/images/boopmas.png"
+                alt="Boopmas.org"
+                width={125}
+                height={100}
+                className="mx-auto md:mx-0"
+                priority
+              />
+            </div>
+            <h1 className="text-3xl md:text-6xl font-bold mb-2 font-christmas md:block">
+              Merry Christmas Boopadoop!
             </h1>
           </div>
         </header>
